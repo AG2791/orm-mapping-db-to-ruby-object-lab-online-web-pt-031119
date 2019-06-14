36c02144d216aafb914f4295f44e6d79f94491dd
AJ Guiles <ashiguiles@gmail.com>
2019-06-14 02:25:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-   new_song = self.new   
-   student.new
+   n = self.new   
+  
   student.id = row[0]
   student.name =  row[1]
   student.grade = row[2]
