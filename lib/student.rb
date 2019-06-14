a95f4682b91d0767108c843fd74c030ce5e0b6a5
AJ Guiles <ashiguiles@gmail.com>
2019-06-14 02:54:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,15 +86,3 @@ end
   end
 
 
-def self.first_X_students_in_grade_10
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE grade = 10
-      ORDER BY students.id
-      LIMIT ?
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-end
