2f3fd149b999fe4cbfa9a2d2d5d18437e0f22dfb
AJ Guiles <ashiguiles@gmail.com>
2019-06-14 02:26:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
    new_student = self.new   
   
   new_student.id = row[0]
-  new_naame =  row[1]
-   = row[2]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
   student
   end
 
